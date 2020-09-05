741e5a7d16405b0028e69b9f467c9e4d1e6c25f8
Bharath Nadampalli <bharath.ns99@gmail.com>
2020-09-05 13:11:45 -0500

    Initial changes

--- SampleProject/SampleProject/main.cpp
+++ SampleProject/SampleProject/main.cpp
@@ -7,7 +7,11 @@
 //
 
 #include <iostream>
+using namespace std;
 
+void foo() {
+    cout<<"Inside foo"<<endl;
+}
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
