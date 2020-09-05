a30e07644161d4bab0964f5747b58eb4aebc2a6e
Bharath Nadampalli <bharath.ns99@gmail.com>
2020-09-05 13:25:36 -0500

    Initial changes

--- SampleProject/SampleProject/main.cpp
+++ SampleProject/SampleProject/main.cpp
@@ -12,6 +12,11 @@ using namespace std;
 void betterFunctionName() {
     cout<<"Inside foo"<<endl;
 }
+
+void mergeSample() {
+    cout<<"Inside merge sample"<<endl;
+}
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
